80a87816de0269187ec3b8bad1f54a31f3ce93ee
Kilian Baur <kilianlbaur@gmail.com>
2020-05-02 00:04:46 +0200

    initial commit vex functions eval Lsystems

---
+++ vex/LEO_evalLSystems.h
@@ -0,0 +1,91 @@
+//common arguments
+//current point
+//current position
+//current direction
+//lineprim
+
+//move position along direction and create geo
+//F move one and create geo
+//H move half and create geo
+function void lsysMoveGeo(vector c_pos, c_dir; float stepsize; int lineprim)
+{
+	c_pos  += c_dir * stepsize;
+	int c_point = addpoint(0, c_pos);
+	addvertex(0, lineprim, c_point);
+}
+
+//move position along direction
+//f move one
+//h move half
+function void lsysMove(vector c_pos, c_dir; float stepsize)
+{
+	c_pos  += c_dir * stepsize;
+}
+
+//rotate and return new c_dir
+// + turn right 
+// - turn left
+function void lsysTurn(vector c_dir, up; float angle)
+{
+	float radians = radians(angle);
+	vector4 quat = quaternion(radians,up);
+	c_dir = qrotate(quat,c_dir);
+}
+
+// " multiply stepsize
+// ! multiply pscale
+// ; multiply angle
+function float lsysMultValue(float origvalue, multiplier)
+{
+	float newvalue = origvalue * multiplier;
+	return newvalue;
+}
+
+// _ multiply stepsize
+// ? multiply pscale
+// @ multiply angle
+function float lsysDivValue(float origvalue, divider)
+{
+	float newvalue = origvalue / divider;
+	return newvalue;
+}
+
+
+
+// ^ pitch up 
+// & pitch down 
+
+// T gravity
+
+// J K M copy geo
+
+// \\ roll clockwise
+// / roll counter clockwise
+
+// | turn 180 degrees
+// * roll 180 degrees
+
+// ~ random value 
+// between 0 and default 180
+
+//////////////////////////////////////////////////////////////////////////////
+//following is probably used in the recursive string generation and not in the interpretion
+
+// [] create branch 
+// % cut of / end branch
+// : conditions
+// ! negate rule
+// : int 
+
+////////////////////////////////////////////////////////////////////////////////
+//parameters
+// g age of current rule (?)
+
+// i offset into the current string (?)
+
+// t iteration count
+
+////////////////////////////////////////////////////////////////////////////////
+//stuff to add
+//limit inside shape
+
