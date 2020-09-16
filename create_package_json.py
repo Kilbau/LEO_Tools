e96c85994cc97b8dde135765b212ec01eada491c
Kilian Baur <kilianlbaur@gmail.com>
2020-09-16 17:15:37 +0200

    pep8 formatting

--- create_package_json.py
+++ create_package_json.py
@@ -1,28 +1,27 @@
 import json
 import os
 
+
+class abc:
+    def __init__(self):
+        pass
+
+
 def run():
-	path = os.getcwd().replace("\\","/")
+    path = os.getcwd().replace("\\", "/")
 
-	content = {
-				"recommends": "houdini_version >= '18.0'",
-				"env":[
-						{"LEO": path},
-						{
-							"PYTHONPATH": "$LEO/scripts/python",
-							"method": "append"
-						}
-					],
-				"path": "$LEO"
+    content = {
+        "recommends": "houdini_version >= '18.0'",
+        "env": [
+            {"LEO": path},
+            {"PYTHONPATH": "$LEO/scripts/python", "method": "append"},
+        ],
+        "path": "$LEO",
+    }
 
-				}
+    with open("LEOTools_package.json", "w") as f:
+        json.dump(content, f, indent=4)
 
-
-	with open("LEOTools_package.json", "w") as f:
-		json.dump(content,f,indent=4)
 
 if __name__ == "__main__":
-	run()
-					
-
-
+    run()
